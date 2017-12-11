4b2f420031a45e54905e2783c0ff3b2c6f8d0f12
maidi1990 <1243001042@qq.com>
2017-12-11 10:39:35 +0800

    提交昨天的修改。
    - 第一次提交。

---
+++ mai.bao/README.txt
@@ -0,0 +1,8 @@
+=========================
+mai.bao package
+=========================
+
+This package is part of the `Expert Python Programming` book  written by 
+Mai Di.
+
+The End.
---
+++ mai.bao/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+long_description = open('README.txt').read()
+classifiers = [
+    "programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+]
+setup(
+    name='mai.bao',
+    version=version,
+    description='this is maidi package templates.',
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords='python best practices',
+    author='MaiDi',
+    author_email='15920164078@163.com',
+    url='https://github.com/Maidi1990/my_bao_template',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['mai'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    test_suite='nose.collector',
+    test_requires=['Nose', 'feedparser'],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    )
